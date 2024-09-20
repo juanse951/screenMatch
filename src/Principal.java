83e03a3eac39d9a85a392b4bce92e9f52dee29ec
juanse951 <juanse951@gmail.com>
2024-09-20 09:55:16 -0500

    Definiendo acciones para la película

--- src/Principal.java
+++ src/Principal.java
@@ -6,17 +6,19 @@ public class Principal {
         miPelicula.fechaDeLanzamiento = 2021;
         miPelicula.duracionEnMinutos = 120;
 
-        System.out.println("Mi película es: " + miPelicula.nombre);
-        System.out.println("Su fecha de lanzamiento es: " + miPelicula.fechaDeLanzamiento);
-        System.out.println("Su duracion es de: " + miPelicula.duracionEnMinutos);
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+
+        System.out.println(miPelicula.sumaDeLasEvaluaciones);
+        System.out.println(miPelicula.totalDeLasEvaluaciones);
+        System.out.println(miPelicula.calculaMedia());
 
         Pelicula otraPelicula = new Pelicula();
         otraPelicula.nombre = "Matrix";
         otraPelicula.fechaDeLanzamiento = 1998;
         otraPelicula.duracionEnMinutos = 180;
 
-        System.out.println("Mi película es: " + otraPelicula.nombre);
-        System.out.println("Su fecha de lanzamiento es: " + otraPelicula.fechaDeLanzamiento);
-        System.out.println("Su duracion es de: " + otraPelicula.duracionEnMinutos);
+       otraPelicula.muestraFichaTecnica();
     }
 }
