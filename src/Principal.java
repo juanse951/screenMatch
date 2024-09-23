b85e1bef41d8baa234a2d8ef4adffc8fa59f03a4
juanse951 <juanse951@gmail.com>
2024-09-22 20:14:21 -0500

    aplicacion de modificadores de acceso y creacion de paquete

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,5 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
+
 public class Principal {
 
     public static void main(String[] args) {
@@ -9,16 +11,21 @@ public class Principal {
         miPelicula.muestraFichaTecnica();
         miPelicula.evalua(10);
         miPelicula.evalua(10);
-
-        System.out.println(miPelicula.sumaDeLasEvaluaciones);
-        System.out.println(miPelicula.totalDeLasEvaluaciones);
+        miPelicula.evalua(7.8);
+        System.out.println(miPelicula.getTotalDeLasEvaluaciones());
         System.out.println(miPelicula.calculaMedia());
 
-        Pelicula otraPelicula = new Pelicula();
-        otraPelicula.nombre = "Matrix";
-        otraPelicula.fechaDeLanzamiento = 1998;
-        otraPelicula.duracionEnMinutos = 180;
 
-       otraPelicula.muestraFichaTecnica();
+
+
+
+
+
+//        com.aluracursos.screenmatch.modelos.Pelicula otraPelicula = new com.aluracursos.screenmatch.modelos.Pelicula();
+//        otraPelicula.nombre = "Matrix";
+//        otraPelicula.fechaDeLanzamiento = 1998;
+//        otraPelicula.duracionEnMinutos = 180;
+//
+//       otraPelicula.muestraFichaTecnica();
     }
 }
